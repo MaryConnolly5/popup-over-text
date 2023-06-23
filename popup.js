484b41496615dd30a754d4fe06ce18a9ddd96760
MaryConnolly5 <112788088+MaryConnolly5@users.noreply.github.com>
2023-06-23 13:48:09 -0400

    changed name

---
+++ popup.js
@@ -0,0 +1,14 @@
+$('.open-popup-words').magnificPopup({
+  type:'inline',
+  midClick: true .
+});
+
+
+// for the FAQ button
+$('button').magnificPopup({
+  items: {
+      src: '<div class="popup1">Dynamically created popup</div>',
+      type: 'inline'
+  },
+  closeBtnInside: true
+});
